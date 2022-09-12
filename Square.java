24b17827b90e8006a81961d6f88a3f31785a9207
AMph1303 <madan.ag@phystech.edu>
2022-09-12 15:10:58 +0300

    Upload hw files

---
+++ Square.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class Square {
+    double length;
+
+    double area() {
+            double Area;
+            Area = Math.pow(length, 2);
+            return Area;
+        }
+    }
+
