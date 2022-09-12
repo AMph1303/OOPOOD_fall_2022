5e2542b8d4eee11694b5bad765157a3366a02908
AMph1303 <madan.ag@phystech.edu>
2022-09-12 14:03:16 +0300

    update file

--- CircleTest.java
+++ CircleTest.java
@@ -1,16 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.lang.Math;
 
 class CircleTest {
     @Test
     void AreaAssertion() {
         Circle circ = new Circle();
-        //circ.radius = 9;
-        //Assertions.assertEquals(1, 1);
-        //List<Integer> numbers = List.of(2, 3, 5, 7);
-        //Assertions.assertEquals(1, numbers.get(0));
-        //Assertions.assertEquals(1, numbers.get(1));
-        //Assertions.assertEquals(1, numbers.get(2));
-        //Assertions.assertEquals(1, numbers.get(3));
+        circ.radius = 2;
+        double s = circ.area();
+        Assertions.assertEquals(Math.PI*2*2, s);
     }
 }
